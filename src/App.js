745e951f2760d8b71e91a8a2703c3b2005928f36
arfakl99 <muhammadarifnust99@gmail.com>
2023-07-18 19:11:51 +0500

    set drawtool update and delte functionality and its styling

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import {
   DrawPolygonMode,
   DrawCircleFromCenterMode,
 } from "react-map-gl-draw";
-import * as turf from "@turf/turf";
-import centroid from "@turf/centroid";
-import { ModifyMode } from "@nebula.gl/edit-modes";
-import './styles/style.css'
+
+import "./styles/style.css";
 import { FaBeer } from "react-icons/fa";
-import Select from "react-dropdown-select";
+import Select, { components } from "react-select";
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2pwY3owbGFxMDVwNTNxcXdwMms2OWtzbiJ9.1PPVl0VLUQgqrosrI2nUhg";
@@ -20,18 +18,23 @@ const MAPBOX_TOKEN =
 const MODES = [
   {
     id: "drawcircle",
-    text: " Radius ",
-    text2: " ⵔ ",
+    text: " Radius",
+    icon: require("./images/circle.png"),
     handler: DrawCircleFromCenterMode,
   },
-  { id: "drawPolygon", text: " Polygon  ⬡", handler: DrawPolygonMode },
+  {
+    id: "drawPolygon",
+    text: " Polygon ",
+    icon: require("./images/polygon.png"),
+    handler: DrawPolygonMode,
+  },
 ];
 
 const DEFAULT_VIEWPORT = {
   width: 800,
   height: 600,
-  longitude: -122.45,
-  latitude: 37.78,
+  longitude: 73.05232198377377,
+  latitude: 33.71650794185521,
   zoom: 14,
 };
 
@@ -39,70 +42,152 @@ const App = () => {
   const [viewport, setViewport] = useState(DEFAULT_VIEWPORT);
   const [modeId, setModeId] = useState(null);
   const [modeHandler, setModeHandler] = useState(null);
+  const [modeId2, setModeId2] = useState(null);
+  const [modeHandler2, setModeHandler2] = useState(null);
+
   const editorRef = useRef();
   const mapRef = useRef();
   const [editedFeatures, setEditedFeatures] = useState("CLOSING");
   const [selectedFeatures, setSelectedFeatures] = useState([]);
+  const [selectedFeature, setSelectedFeature] = useState(null);
+  const [currentShape, setCurrentShape] = useState(null);
+  const [drawnFeaturesInfo, setDrawnFeaturesInfo] = useState([]);
+  const [drawingComplete, setDrawingComplete] = useState(false);
 
-  const switchMode = (evt) => {
-    const newModeId = evt.target.value === modeId ? null : evt.target.value;
+  
+
+  const switchMode2 = (newValue) => {
+    const newModeId = newValue.value;
     const mode = MODES.find((m) => m.id === newModeId);
     const newModeHandler = mode ? new mode.handler() : null;
-    setModeId(newModeId);
-    setModeHandler(newModeHandler);
+
+    if (newModeHandler instanceof DrawCircleFromCenterMode) {
+      setModeHandler(new DrawCircleFromCenterMode());
+    } else if (newModeHandler instanceof DrawPolygonMode) {
+      setModeHandler(new DrawPolygonMode());
+    }
+
+    setModeId2(newModeId);
+    debugger;
   };
 
-  const CustomDropdown = ({ icon, text }) => (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      {icon}
-      <span style={{ marginLeft: 5 }}>{text}</span>
-    </div>
+  const options = MODES.map((mode) => ({
+    value: mode.id,
+    label: mode.text,
+    icon: mode.icon,
+  }));
+
+  const IconOption = (props) => (
+    <components.Option {...props}>
+      <img src={props.data.icon} style={{ width: 24 }} alt={props.data.label} />
+      <span style={{ marginLeft: 8 }}>{props.data.label}</span>
+    </components.Option>
   );
-  
+
+  const MultiValueOption = (props) => (
+    <components.MultiValue {...props}>
+      <img src={props.data.icon} style={{ width: 36 }} alt={props.data.label} />
+      <span>{props.data.label}</span>
+    </components.MultiValue>
+  );
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? "#969b89" : "white",
+      color: state.isSelected ? "white" : "#969b89",
+      cursor: "pointer",
+      display: "flex", // Make the option content inline
+      alignItems: "center", // Center the icon and text vertically
+    }),
+    optionLabel: {
+      
+      flex: 1, // Allow the label to take up remaining space
+    },
+    optionIcon: { 
+      width: 10,
+      height: 10, 
+      marginRight: 10, 
+    },
+    
+  };
   const renderToolbar = () => {
+    const selectedOption2 = options.find((option) => option.value === modeId2);
     return (
       <>
         <div style={{ position: "absolute", top: 0, right: 100, padding: 4 }}>
           <button
-            style={{ color: "#969b89", marginRight: 20, marginTop: 20 }}
+            style={{ color: "#969b89", marginRight: 20, marginTop: 10,width:70, height:30 }}
             onClick={handleDelete}
           >
             Delete
           </button>
         </div>
-  
-        <div style={{ position: "absolute", top: 0, right: 0, maxWidth: "420px" }}>
-          <select
-            style={{ color: "#969b89", marginRight: 20, marginTop: 20, padding: 4 }}
-            onChange={switchMode}
-          >
-            <option value="draw">Draw</option>
-            {MODES.map((mode) => (
-              <option key={mode.id} value={mode.id}>
-                {mode.text === " Radius " ? (
-                  <CustomDropdown icon={<FaBeer />} text={mode.text} />
-                ) : (
-                  mode.text
-                )}
-              </option>
-            ))}
-          </select>
+
+        {/* div2 */}
+        <div
+          style={{
+            position: "absolute",
+            top: 10,
+            right: 20,
+            maxWidth: "420px",
+          }}
+        >
+          <Select
+            placeholder={"Draw"}
+            value={selectedOption2}
+            onChange={switchMode2}
+            options={options}
+            components={{
+              Option: IconOption,
+              MultiValue: MultiValueOption,
+            }}
+            styles={customStyles}
+          />
         </div>
       </>
     );
   };
-   
 
   const handleDelete = () => {
     if (editorRef.current) {
-      console.log(editorRef.current.deleteFeatures([selectedFeatures]));
+      editorRef.current.deleteFeatures([selectedFeatures]);
     }
-    setSelectedFeatures([]);
-    setViewport(DEFAULT_VIEWPORT);
+    // setSelectedFeatures([]);
+    // setViewport(DEFAULT_VIEWPORT);
+  };
+
+  const handleFeatureUpdate = (features) => {
+    console.log("All Drawn Feature Info:", features);
+    if (features.data.length > 1) {
+      const lastFeature = features.data[features.data.length - 1];
+      editorRef.current.deleteFeatures(0);
+      setTimeout(() => editorRef.current.addFeatures(lastFeature), 100);
+
+      //
+      // editorRef.current.deleteFeatures(features.data[0]);
+      // editorRef.current.
+      // setDrawnFeaturesInfo([lastFeature]);
+    } else {
+      setDrawnFeaturesInfo(features.data);
+    }
   };
 
   const handleSelect = (selectedFeatures) => {
-    console.log("Draw Features:", selectedFeatures.mapCoords);
+    console.log("selectefFeature are " + selectedFeatures);
+    if (selectedFeatures.length > 0) {
+      const selectedFeature = selectedFeatures[0];
+      console.log("Selected Features:", selectedFeatures);
+
+      if (currentShape) {
+        if (editorRef.current) {
+          editorRef.current.deleteFeatures([currentShape]);
+        }
+      }
+
+      setCurrentShape(selectedFeature);
+      console.log("Current Shape:", currentShape);
+    }
   };
 
   return (
@@ -121,6 +206,8 @@ const App = () => {
           clickRadius={12}
           mode={modeHandler}
           onSelect={handleSelect}
+          // onUpdate={handleGetFeatures}
+          onUpdate={handleFeatureUpdate}
         />
         {renderToolbar()}
       </MapGL>
